4d3dbed3e5511923acf860c5d891cf03e59a6ced
Sebastian Rautenbach <55651172+SebastianRautenbach@users.noreply.github.com>
2024-03-12 18:36:37 +0200

    directional light fix

--- RenderEngine/src/components/directionallight_component.cpp
+++ RenderEngine/src/components/directionallight_component.cpp
@@ -3,9 +3,9 @@
 wizm::directionallight_component::directionallight_component( glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular)
 {
 		m_component_type = eDirectionalLight;
-		glm::vec3 m_ambient = ambient;
-		glm::vec3 m_diffuse = diffuse;
-		glm::vec3 m_specular = specular;
+		m_ambient = ambient;
+		m_diffuse = diffuse;
+		m_specular = specular;
 }
 
 void wizm::directionallight_component::component_preupdate()
@@ -15,9 +15,9 @@ void wizm::directionallight_component::component_preupdate()
 void wizm::directionallight_component::component_update()
 {
 	shader->setVec3("dirLight.direction", m_rotation);
-	shader->setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-	shader->setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-	shader->setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+	shader->setVec3("dirLight.ambient", m_ambient);
+	shader->setVec3("dirLight.diffuse", m_diffuse);
+	shader->setVec3("dirLight.specular", m_specular);
 }
 
 void wizm::directionallight_component::component_postupdate()
